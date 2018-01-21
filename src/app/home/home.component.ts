63f1260c1ba71fcc3ad4f5102d857db319ba07f2
Fabian Wiles <fabianwiles@live.com>
2018-01-21 21:50:22 +1300

    observable content demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'home',
-  template: `<h3>{{ message }}</h3>`
+  template: `<h3>{{ message | async }}</h3>`
 })
 export class HomeComponent implements OnInit {
-  public message: string;
+  public message: Observable<string>;
 
   constructor() {}
 
   ngOnInit() {
-    this.message = 'Hello';
+    this.message = of('some async message').pipe(delay(2000))
   }
 }
